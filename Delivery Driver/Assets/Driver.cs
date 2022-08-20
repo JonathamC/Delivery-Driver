a8aee32d4c4a1e7dbf8c5017e4f9dc19bba53f13
JonathamC <86174437+JonathamC@users.noreply.github.com>
2022-08-20 19:21:59 -0400

    implemented move features

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     
+    // Declaring variables 
+    [SerializeField] float steerSpeed = 10f; 
+    [SerializeField] float moveSpeed = 1f; 
+
+
     void Start()
     {
         
@@ -12,7 +17,13 @@ public class Driver : MonoBehaviour
 
     
     void Update()
-    {
-        transform.Rotate(0, 0, 0.1f);
+    {   
+        // add Time.delaTime to make frame rate independant -> slow and fast computer both perform the same 
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        // Rotation
+       transform.Rotate(0, 0, -steerAmount);
+       // Move forward
+       transform.Translate(0, moveAmount, 0);
     }
 }
